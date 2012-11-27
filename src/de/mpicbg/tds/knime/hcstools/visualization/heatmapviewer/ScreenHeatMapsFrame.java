0936d58fdc7d3d5fb013f80c6e2cbd092fb8fdce
felix <nibelungen@gmx.ch>
2012-11-27 16:31:59 +0100

    Add a test class for the PlateHeatMapViewer (ScreenHeatMapsFrame)
    This provides a tool to quickly run tests within the IDE and without having to compile and launch KNIME each time. Executing the PlateHeatMapViewerTest classes main() will do the trick from now on.
    - For this pupose I create a new "test" source directory
    - I add test data (this should probably be moved to a server and linked by url at some point)
    - clean out the code for testing the ScreenHeatMapsFrame class
    - add a second constructure for the ScreenHeatMapsFrame class that allows to pass the data from the file.

--- src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ScreenHeatMapsFrame.java
+++ src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ScreenHeatMapsFrame.java
@@ -1,22 +1,14 @@
 package de.mpicbg.tds.knime.hcstools.visualization.heatmapviewer;
 
-import de.mpicbg.tds.core.TdsUtils;
+import de.mpicbg.tds.core.model.Plate;
 import de.mpicbg.tds.knime.hcstools.visualization.heatmapviewer.color.ColorBar;
-import de.mpicbg.tds.knime.knutils.Attribute;
-import org.knime.core.data.*;
-import org.knime.core.data.container.ContainerTable;
-import org.knime.core.data.container.DataContainer;
-import org.knime.core.node.*;
-import org.knime.core.node.config.Config;
+
 import org.knime.core.node.property.hilite.HiLiteListener;
 import org.knime.core.node.property.hilite.KeyEvent;
-import org.openscience.jchempaint.renderer.generators.ControllerFeedbackGenerator;
 
 import javax.swing.*;
-import javax.swing.text.TableView;
 import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.util.List;
 
 /**
  * User: Felix Meyenhofer
@@ -38,6 +30,22 @@ public class ScreenHeatMapsFrame extends JFrame implements HiLiteListener{
         setTitle("HCS Heat-map Viewer");
         menus = new HeatMapMenu();
         screenPanel = new ScreenPanel();
+        toolbar = new HeatMapToolbar();
+        colorbar = new ColorBar();
+        setJMenuBar(menus);
+        add(screenPanel, BorderLayout.CENTER);
+        add(toolbar, BorderLayout.NORTH);
+        add(colorbar, BorderLayout.SOUTH);
+        setBounds(150, 150, 800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+    public ScreenHeatMapsFrame(List<Plate> plates) {
+        setTitle("HCS Heat-map Viewer");
+        menus = new HeatMapMenu();
+        screenPanel = new ScreenPanel(plates);
         toolbar = new HeatMapToolbar();
         colorbar = new ColorBar();
         setJMenuBar(menus);
@@ -78,38 +86,5 @@ public class ScreenHeatMapsFrame extends JFrame implements HiLiteListener{
 
     public static void main(String[] args) {
         new ScreenHeatMapsFrame();
-
-//        // Read the table spec.
-//        FileInputStream stream = null;
-//        try {
-//            stream = new FileInputStream(new File("/Users/turf/Sources/CBG/HCS-Tools/test/spec.xml"));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        NodeSettings settings = null;
-//        try {
-//            settings = (NodeSettings) NodeSettings.loadFromXML(stream);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        DataTableSpec specs = null;
-//        try {
-//            specs = DataTableSpec.load(settings);
-//        } catch (InvalidSettingsException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Read the table data.
-//        DataContainer container = new DataContainer(specs);
-//        ContainerTable output = null;
-//        try {
-//            output = container.readFromZip(new File("/Users/turf/Sources/CBG/HCS-Tools/test/data.zip"));
-//        } catch (IOException e) {
-//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//        }
-
-        System.exit(0);
     }
 }
