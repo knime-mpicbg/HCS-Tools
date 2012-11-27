ef5b5b07720e8483df31c2bfff0c143940015636
felix <nibelungen@gmx.ch>
2012-11-27 13:34:45 +0100

    Attempt to load the table (as saved from the node in a workflow) and parse it to the GUI. Outside of the execution context of KNIME (loaded workflow) I could not find a way to create a BufferedDataTable.

--- src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ScreenHeatMapsFrame.java
+++ src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ScreenHeatMapsFrame.java
@@ -1,11 +1,22 @@
 package de.mpicbg.tds.knime.hcstools.visualization.heatmapviewer;
 
+import de.mpicbg.tds.core.TdsUtils;
 import de.mpicbg.tds.knime.hcstools.visualization.heatmapviewer.color.ColorBar;
+import de.mpicbg.tds.knime.knutils.Attribute;
+import org.knime.core.data.*;
+import org.knime.core.data.container.ContainerTable;
+import org.knime.core.data.container.DataContainer;
+import org.knime.core.node.*;
+import org.knime.core.node.config.Config;
 import org.knime.core.node.property.hilite.HiLiteListener;
 import org.knime.core.node.property.hilite.KeyEvent;
+import org.openscience.jchempaint.renderer.generators.ControllerFeedbackGenerator;
 
 import javax.swing.*;
+import javax.swing.text.TableView;
 import java.awt.*;
+import java.io.*;
+import java.util.*;
 
 /**
  * User: Felix Meyenhofer
@@ -67,5 +78,38 @@ public class ScreenHeatMapsFrame extends JFrame implements HiLiteListener{
 
     public static void main(String[] args) {
         new ScreenHeatMapsFrame();
+
+//        // Read the table spec.
+//        FileInputStream stream = null;
+//        try {
+//            stream = new FileInputStream(new File("/Users/turf/Sources/CBG/HCS-Tools/test/spec.xml"));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        NodeSettings settings = null;
+//        try {
+//            settings = (NodeSettings) NodeSettings.loadFromXML(stream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        DataTableSpec specs = null;
+//        try {
+//            specs = DataTableSpec.load(settings);
+//        } catch (InvalidSettingsException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Read the table data.
+//        DataContainer container = new DataContainer(specs);
+//        ContainerTable output = null;
+//        try {
+//            output = container.readFromZip(new File("/Users/turf/Sources/CBG/HCS-Tools/test/data.zip"));
+//        } catch (IOException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+
+        System.exit(0);
     }
 }
