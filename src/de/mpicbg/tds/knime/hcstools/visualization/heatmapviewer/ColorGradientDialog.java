dba8d22c906fd4de543c40779c118f0c448ecd6e
felix <nibelungen@gmx.ch>
2012-12-08 00:33:14 +0100

    change libraries of the ColorGradientDialog
    Found the ColorGradientSlider and use that com.bric.swing package instead (simpler interface, easier to use)
    add body of the onOk method.
    do a lot of refactoring and cleaning of the code.

--- src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ColorGradientDialog.java
+++ src/de/mpicbg/tds/knime/hcstools/visualization/heatmapviewer/ColorGradientDialog.java
@@ -1,44 +1,84 @@
 package de.mpicbg.tds.knime.hcstools.visualization.heatmapviewer;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
+import java.awt.geom.Point2D;
 
-import org.jdesktop.swingx.JXGradientChooser;
-import org.jdesktop.swingx.JXPanel;
+import com.bric.swing.GradientSlider;
+import com.bric.swing.MultiThumbSlider;
 
 
 public class ColorGradientDialog extends JDialog {
 
-    private JPanel contentPane, mainPanel, buttonPanel;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-    private JXGradientChooser gradientChooser;
-
-    private Component[] components;
+    private GradientSlider slider;
+    private LinearGradientPaint currentGradient;
 
 
+    // Constructor
     public ColorGradientDialog() {
-        this.initialize();
-        setContentPane(contentPane);
+        setContentPane(initialize());
+        setSize(new Dimension(500, 200));
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+    }
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
+    public ColorGradientDialog(String title) {
+        this();
+        setTitle(title);
+    }
 
+
+    // Utilities
+    private JPanel initialize() {
+        // Create the cancel and ok buttons in a seperate panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.setBorder(BorderFactory.createEtchedBorder());
+        JButton buttonCancel = new JButton("Cancel");
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
         });
+        buttonPanel.add(buttonCancel);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        JButton buttonOK = new JButton("OK");
+        getRootPane().setDefaultButton(buttonOK);
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+        buttonPanel.add(buttonOK);
 
-// call onCancel() when cross is clicked
+        // Create the Gradient slider
+        slider = new GradientSlider(MultiThumbSlider.HORIZONTAL);
+        Color leftColor = new Color(0,255,0);
+        Color middleColor = new Color(0,0,0);
+        Color rightColor = new Color(255, 0,0);
+        slider.setValues(new float[] {0, (float) 0.5, 1}, new Color[] { leftColor, middleColor, rightColor});
+        slider.setPaintTicks(true);
+        slider.setBorder(BorderFactory.createEtchedBorder());
+        slider.putClientProperty("MultiThumbSlider.indicateComponent", "false");
+        slider.putClientProperty("GradientSlider.useBevel", "true");
+        slider.setToolTipText("<html>click on the thumbs to select them<br/>dragg the thumbs to slide or remove them<br/>" +
+                "double click on a thumb to choose it's color<html>");
+
+        // Create the content paine and layout the buttons and the slider.
+        JPanel contentPane = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10,10,10,10);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1;
+        constraints.gridy = 0;
+        constraints.weighty = 0.8;
+        contentPane.add(slider, constraints);
+        constraints.gridy = 1;
+        constraints.weighty = 0.2;
+        contentPane.add(buttonPanel, constraints);
+
+        // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -46,49 +86,39 @@ public class ColorGradientDialog extends JDialog {
             }
         });
 
-// call onCancel() on ESCAPE
+        // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        return contentPane;
     }
 
+    public LinearGradientPaint getGradientPainter() {
+        return currentGradient;
+    }
+
+
+    // Actions
     private void onOK() {
-// add your code here
+        Color[] colors = slider.getColors();
+        float[] positions = slider.getThumbPositions();
+        Point2D startPoint = new Point2D.Double(0,0);
+        Point2D endPoint = new Point2D.Double(255,0);
+        currentGradient = new LinearGradientPaint(startPoint, endPoint, positions, colors);
         dispose();
     }
 
     private void onCancel() {
-// add your code here if necessary
         dispose();
     }
 
-    private void initialize() {
-        buttonPanel = new JPanel();
-        buttonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        buttonCancel = new JButton("Cancel");
-        buttonOK = new JButton("OK");
-        buttonPanel.add(buttonCancel);
-        buttonPanel.add(buttonOK);
 
-        mainPanel = new JPanel();
-        gradientChooser = new JXGradientChooser();
-//        components = gradientChooser.getRootPane().getContentPane().getComponents();
-//        for (int i = 0; i < components.length; i++) {
-//            System.out.println(components[i].getName());
-//        }
-        mainPanel.add(gradientChooser);
-
-        contentPane = new JPanel();
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        contentPane.add(mainPanel);
-        contentPane.add(buttonPanel);
-    }
-
+    // Reveal yourself!
     public static void main(String[] args) {
-        ColorGradientDialog dialog = new ColorGradientDialog();
-        dialog.pack();
+        ColorGradientDialog dialog = new ColorGradientDialog("Color gradient dialog test");
         dialog.setVisible(true);
         System.exit(0);
     }
