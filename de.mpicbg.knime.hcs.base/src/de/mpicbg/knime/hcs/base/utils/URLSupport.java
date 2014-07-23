a6200359168031525fcfbba403b04f07f48f7fd2
Antje Janosch <niederle@mpi-cbg.de>
2014-07-23 14:14:50 +0200

    URLSupport taken from src to its new location

--- de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/utils/URLSupport.java
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/utils/URLSupport.java
@@ -1,4 +1,7 @@
-package de.mpicbg.knime.hcs.base.utils;
+/**
+ * 
+ */
+package de.mpicbg.tds.knime.hcstools.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,63 +10,62 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
-
 /**
-* @author niederle
-*
-* these methods are meant to support the KNIME-protocol for URLs
-* it's important to place this class within KNIME, as the KNIME-protocol is unknown otherwise
-* and will take care of interpreting local style as file://
-*
-*/
+ * @author niederle
+ * 
+ * these methods are meant to support the KNIME-protocol for URLs
+ * it's important to place this class within KNIME, as the KNIME-protocol is unknown otherwise
+ * and will take care of interpreting local style as file://
+ *
+ */
 public class URLSupport {
-
-/**
-* URL connection
-*/
-private URLConnection connection;
-
-/**
-* default constructor
-*/
-public URLSupport() {
-this.connection = null;	
-}
-
-/**
-* tries to create a URL connection by a given file name
-* it adds file:// if the protocol is not given to treat it as local
-* @param filename
-* @throws URISyntaxException
-* @throws IOException
-*/
-public URLSupport(String filename) throws URISyntaxException, IOException {
-this.connection = null;
-     //check if URL can be resolved
-     // retrieve protocol
-     String protocol = (new URI(filename)).getScheme();
-     // try to add file-protocol to treat it as a local file
-     if(protocol == null) filename = "file://" + filename;
-    
-URL fileURL = new URL(filename);
-this.connection = fileURL.openConnection();
-}
-
-/**
-* @return timestamp of the last modification time
-*/
+	
+	/**
+	 * URL connection
+	 */
+	private URLConnection connection;
+	
+	/**
+	 * default constructor
+	 */
+	public URLSupport() {
+		this.connection = null;		
+	}
+	
+	/**
+	 * tries to create a URL connection by a given file name
+	 * it adds file:// if the protocol is not given to treat it as local
+	 * @param filename
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 */
+	public URLSupport(String filename) throws URISyntaxException, IOException {
+		this.connection = null;
+    	//check if URL can be resolved
+    	// retrieve protocol
+    	String protocol = (new URI(filename)).getScheme();
+    	// try to add file-protocol to treat it as a local file
+    	if(protocol == null) filename = "file://" + filename;
+    		
+		URL fileURL = new URL(filename);
+		this.connection = fileURL.openConnection();
+	}
+	
+	/**
+	 * @return timestamp of the last modification time
+	 */
     public long getTimestamp() {
-     return this.connection.getLastModified();
+    	return this.connection.getLastModified();
     }
     
     /**
-*
-* @return InputStream of this URL connection
-* @throws IOException
-*/
+     * 
+     * @return InputStream of this URL connection
+     * @throws IOException
+     */
     public InputStream getInputStream() throws IOException {
-     if(this.connection == null) return null;
-     return this.connection.getInputStream();
+    	if(this.connection == null) return null;
+    	return this.connection.getInputStream();
     }
 
 }
