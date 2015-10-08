ae9bd65704cc2c0e4a3eebaf9eecefeb726120d3
timnicolaisen <tim.nicolaisen@htw-dresden.de>
2015-10-08 13:04:23 +0200

    First Steps - Creating new Column - Convert two specific Columns

--- de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/layout/createwellposition/CreateWellPositionNodeModel.java
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/layout/createwellposition/CreateWellPositionNodeModel.java
@@ -1,9 +1,28 @@
 package de.mpicbg.knime.hcs.base.nodes.layout.createwellposition;
+import de.mpicbg.knime.knutils.AbstractNodeModel;
+import de.mpicbg.knime.knutils.Attribute;
+import de.mpicbg.knime.knutils.InputTableAttribute;
+import de.mpicbg.knime.knutils.TableUpdateCache;
 
 import java.io.File;
 import java.io.IOException;
 
+import org.dmg.pmml.DATATYPE;
+import org.knime.base.node.io.filereader.DataCellFactory;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.DoubleValue;
+import org.knime.core.data.IntValue;
+import org.knime.core.data.container.CellFactory;
+import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.container.RowAppender;
+import org.knime.core.data.container.SingleCellFactory;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.StringCell;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -12,8 +31,14 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModel;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
-import de.mpicbg.knime.hcs.base.nodes.layout.expandbarcode2.String;
+import de.mpicbg.knime.hcs.base.nodes.layout.ExpandWellPositionFactory;
+import de.mpicbg.knime.hcs.base.nodes.layout.expandbarcode2.ExpandPlateBarcodeModel2;
+import de.mpicbg.knime.hcs.base.nodes.norm.AbstractScreenTrafoModel;
+import de.mpicbg.knime.hcs.core.TdsUtils;
 
 /**
  * The node should be able to create a new string column with the well position, given two input columns with the plate row (numeric or alphabetic) and plate column (numeric) information.
@@ -28,7 +53,15 @@ public class CreateWellPositionNodeModel extends AbstractNodeModel {
 	
 	// NODE SETTINGS KEYS + DEFAULTS
 	
-	public static final String CFG_BARCODE_COLUMN = "barcode.column";
+	public static final String CFG_PlateColumn =  "plate.column";
+	public static final String CFG_PlateColumn_DFT = "plateColumn";
+	
+	public static final String CFG_PlateRow = "plate.row";
+	public static final String CFG_PlateRow_DFT = "plateRow";
+	
+	public static final String CFG_deleteSouceCol = "delete.source.column";
+	public static final String CFG_deleteSouceRow = "delete.source.row";
+	private static final DataType String = null;
 	
 	
 	
@@ -40,19 +73,86 @@ public class CreateWellPositionNodeModel extends AbstractNodeModel {
     
         // TODO: Specify the amount of input and output ports needed.
         super(1, 1);
+        addModelSetting(CreateWellPositionNodeModel.CFG_PlateColumn, createPlateColumn());
+		addModelSetting(CreateWellPositionNodeModel.CFG_PlateColumn, createPlateRow());
+		addModelSetting(CreateWellPositionNodeModel.CFG_deleteSouceCol, createPlateColumn());
+		addModelSetting(CreateWellPositionNodeModel.CFG_deleteSouceRow, createDelSourceRow());
     }
+    
+    private SettingsModel createPlateColumn() {
+		return new SettingsModelString( CFG_PlateColumn, null);
+	}
+    
+    private SettingsModel createPlateRow() {
+		return new SettingsModelString(CFG_PlateRow, null);
+	}
 
+    private SettingsModel createDelSourceCol() {
+		return new SettingsModelBoolean(CFG_deleteSouceCol, false);
+	}
+    
+    private SettingsModel createDelSourceRow() {
+		return new SettingsModelBoolean(CFG_deleteSouceRow, false);
+	}
+    
+    
+    
     /**
      * {@inheritDoc}
      */
     @Override
-    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
-            final ExecutionContext exec) throws Exception {
-    		
-    	
-        // TODO: Return a BufferedDataTable for each output port 
-        return new BufferedDataTable[]{};
-    }
+    public BufferedDataTable[] execute(BufferedDataTable[] in,
+    	     ExecutionContext exec) throws Exception {
+    	     ColumnRearranger c = createColumnRearranger(in[0].getDataTableSpec());
+    	     BufferedDataTable out = exec.createColumnRearrangeTable(in[0], c, exec);
+    	     return new BufferedDataTable[]{out};
+    	 }
+
+    	 public DataTableSpec[] configure(DataTableSpec[] in)
+    	         throws InvalidSettingsException {
+    	     DataColumnSpec c0 = in[0].getColumnSpec(3);
+    	     DataColumnSpec c1 = in[0].getColumnSpec(2);
+    	     if (!c0.getType().isCompatible(DoubleValue.class)) {
+    	         throw new InvalidSettingsException(
+    	           "Invalid type at first column.");
+    	     }
+    	     if (!c1.getType().isCompatible(DoubleValue.class)) {
+    	         throw new InvalidSettingsException(
+    	           "Invalid type at second column.");
+    	     }
+    	     ColumnRearranger c = createColumnRearranger(in[0]);
+    	     DataTableSpec result = c.createSpec();
+    	     return new DataTableSpec[]{result};
+    	 }
+    	 
+
+    
+
+    	 private ColumnRearranger createColumnRearranger(DataTableSpec in) {
+    	     ColumnRearranger c = new ColumnRearranger(in);
+    	     // column spec of the appended column
+    	     DataColumnSpec newColSpec = new DataColumnSpecCreator(
+    	     "WellPosition", StringCell.TYPE).createSpec();
+    	     // utility object that performs the calculation
+    	     CellFactory factory = new SingleCellFactory(newColSpec) {
+    	         public DataCell getCell(DataRow row) {
+    	             DataCell c0 = row.getCell(1);
+    	             DataCell c1 = row.getCell(2);
+    	             if (c0.isMissing() || c1.isMissing()) {
+    	                 return DataType.getMissingCell();
+    	             } else {
+    	                 // configure method has checked if column 0 and 1 are numeric
+    	                 // safe to type cast
+    	                 String d0 = TdsUtils.mapPlateRowNumberToString(((IntValue)c0).getIntValue());
+    	                 String d1 = c1.toString();
+    	                 return new StringCell(d0.concat(d1));
+    	             }
+    	         }
+    	     };
+    	     c.append(factory);
+    	     return c;
+    	 }
+    	 
 
     /**
      * {@inheritDoc}
@@ -62,17 +162,7 @@ public class CreateWellPositionNodeModel extends AbstractNodeModel {
         // TODO: generated method stub
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
-            throws InvalidSettingsException {
-
-        // TODO: generated method stub
-        return new DataTableSpec[]{null};
-    }
-
+    
     /**
      * {@inheritDoc}
      */
