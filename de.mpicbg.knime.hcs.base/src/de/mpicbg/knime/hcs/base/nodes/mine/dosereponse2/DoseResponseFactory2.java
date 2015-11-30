445d1771029e92260b12e07ebc24798cf2660d46
Antje Janosch <niederle@mpi-cbg.de>
2015-11-30 09:14:54 +0100

    Improve Dose Response: Create a new version of the node and change the R code to extract more parameters (slope, min, max) and add an image port to the node

---
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/mine/dosereponse2/DoseResponseFactory2.java
@@ -0,0 +1,118 @@
+package de.mpicbg.knime.hcs.base.nodes.mine.dosereponse2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import org.knime.core.data.DataType;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.data.image.png.PNGImageContent;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.image.ImagePortObject;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.Rserve.RConnection;
+
+import de.mpicbg.knime.knutils.AbstractNodeModel;
+import de.mpicbg.knime.knutils.BufTableUtils;
+import de.mpicbg.knime.scripting.core.rgg.TemplateUtils;
+import de.mpicbg.knime.scripting.r.RSnippetNodeModel;
+import de.mpicbg.knime.scripting.r.RUtils;
+import de.mpicbg.knime.scripting.r.generic.GenericRPlotNodeModel;
+import de.mpicbg.knime.scripting.r.generic.RPortObject;
+import de.mpicbg.knime.scripting.r.generic.RPortObjectSpec;
+import de.mpicbg.knime.scripting.r.plots.RPlotCanvas;
+import de.mpicbg.knime.scripting.r.rgg.HardwiredGenericRPlotNodeFactory;
+
+public class DoseResponseFactory2 extends HardwiredGenericRPlotNodeFactory {
+
+	public String getTemplateFileName() {
+        return "DoseResponse.rgg";
+    }
+
+    private static PortType[] createOutputPorts() {
+        PortType[] tablePort = AbstractNodeModel.createPorts(1);
+        PortType[] modelPort = AbstractNodeModel.createPorts(1, RPortObject.TYPE, RPortObject.class);
+
+        return new PortType[]{tablePort[0], ImagePortObject.TYPE, modelPort[0]};
+    }
+
+	@Override
+	protected GenericRPlotNodeModel createNodeModelInternal() {
+		return new GenericRPlotNodeModel(AbstractNodeModel.createPorts(1), createOutputPorts()) {
+
+            protected PortObject[] prepareOutput(ExecutionContext exec, RConnection connection) {
+                try {
+                    // prepare the output model  (table as well as model port)
+                    REXP resultTable = connection.eval("ictable");
+                    BufferedDataTable dataTable = RUtils.convert2DataTable(exec, resultTable, null);
+
+                    File rWorkspaceFile = File.createTempFile("genericR", "R");
+                    RUtils.saveToLocalFile(rWorkspaceFile, connection, RUtils.getHost(), RSnippetNodeModel.R_OUTVAR_BASE_NAME);
+                    
+                    // Rerun the image
+                    PNGImageContent content;
+                    File m_imageFile = File.createTempFile("RImage", ".png");
+                    ImageIO.write(RPlotCanvas.toBufferedImage(image), "png", m_imageFile);
+                    FileInputStream in = new FileInputStream(m_imageFile);
+                    content = new PNGImageContent(in);
+                    in.close();
+
+                    PortObject imgPort = new ImagePortObject(content, IM_PORT_SPEC);
+
+                    return new PortObject[]{dataTable, imgPort, new RPortObject(rWorkspaceFile)};
+
+                } catch (Throwable e) {
+                    throw new RuntimeException("Could not save rmodel: " + e);
+                }
+            }
+
+
+            @Override
+            protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+
+                // get the parameter value from the ui
+                Map<String, Object> getTemplateConfig = getTemplateConfig(inSpecs);
+
+                Object o = getTemplateConfig.get("Compound");
+                String compoundName = o != null ? o.toString() : "readout";
+                
+                LinkedHashMap<String, DataType> newColumns = new LinkedHashMap<String, DataType>();
+                newColumns.put(compoundName, StringCell.TYPE);
+                newColumns.put("Response Variable", StringCell.TYPE);
+                newColumns.put("IC 50", DoubleCell.TYPE);
+                newColumns.put("Std. Error (IC 50)", DoubleCell.TYPE);
+                newColumns.put("Lower Limit", DoubleCell.TYPE);
+                newColumns.put("Std. Error (Lower Limit)", DoubleCell.TYPE);
+                newColumns.put("Upper Limit", DoubleCell.TYPE);
+                newColumns.put("Std. Error (Upper Limit)", DoubleCell.TYPE);
+                newColumns.put("Slope", DoubleCell.TYPE);
+                newColumns.put("Std. Error (Slope)", DoubleCell.TYPE);
+                newColumns.put("Symmetry", DoubleCell.TYPE);
+                newColumns.put("Std. Error (Symmetry)", DoubleCell.TYPE);
+                newColumns.put("Residual standard error", DoubleCell.TYPE);
+                newColumns.put("DoF", IntCell.TYPE);
+                
+                return new PortObjectSpec[]{BufTableUtils.createNewDataTableSpec(newColumns), IM_PORT_SPEC, RPortObjectSpec.INSTANCE};
+            }
+
+
+            @Override
+            public String prepareScript() {
+                String utilsFuns = TemplateUtils.readResourceAsString(this, "drcutils.R");
+
+                return utilsFuns + super.prepareScript();
+            }
+        };
+	}
+
+}
---
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/mine/dosereponse2/drcutils.R
@@ -0,0 +1,311 @@
+library(drc)
+library(ggplot2)
+
+# This needs to be used if the drc-plotting mechanism should be used
+calcDRModels <- function(drdata, readoutIndices, treatmentIndex, concIndex, model = LL.4(), ...){
+
+
+    conditions <- unique(drdata[, treatmentIndex])
+
+
+	# readoutIndex = readoutIndices[1]
+	drmodels <- lapply(readoutIndices, FUN = function(readoutIndex){
+		#readoutIndex=1
+	    subDRData <- data.frame(meas = drdata[,readoutIndex], treatment = drdata[,treatmentIndex], conc = drdata[,concIndex])
+		drcmodel <- try(drm(meas ~ conc, treatment, data= subDRData, fct= model), silent=TRUE, ...)
+		if(inherits(drcmodel, "try-error")){
+			return(subDRData)
+		}else{
+			# put it into our output datastrucutre
+			return(drcmodel)
+		}
+	})
+
+	names(drmodels) <- names(drdata)[readoutIndices]
+
+    return(drmodels)
+}
+
+
+
+# This needs to be used if need an easier access to the plot parameters.
+calcDRModelsListList <- function(drdata, readoutIndices, treatmentIndex, concIndex, model = LL.4(), constraints = list(NULL, NULL), ...){
+
+    conditions <- unique(drdata[, treatmentIndex])
+	#browser()
+	drmodels <- lapply(conditions, FUN = function(con){
+
+		drmodel <- lapply(readoutIndices, FUN = function(readoutIndex){
+    	    # create a temporary dataset
+			subDRData <- drdata[ drdata[, treatmentIndex] == con,c(readoutIndex,concIndex)]
+    	    names(subDRData) <- c("meas", "conc")
+			
+            ## did not work as expected so we just disabled it
+            # lowerParConstr = constraints[[1]]
+            # upperParConstr = constraints[[2]]
+            # drcmodel <- try(drm(meas ~ conc, data= subDRData, fct= model, lowerl=c(10, 50,-Inf,-Inf), upperl=upperParConstr, ...))
+
+			drcmodel <- tryCatch( { 
+				drcmodel <- drm(meas ~ conc, data= subDRData, fct= model, ...)
+				predict(drcmodel)
+				drcmodel
+			}, error = function(e) { NA } )
+
+		})
+
+		names(drmodel) <- names(drdata)[readoutIndices]
+		return(drmodel)
+    })
+
+    names(drmodels) <- conditions
+    return(drmodels)
+}
+
+
+
+#######
+# Inverts the structure of a dr-model table (form conditions in rows and respVars in columns to vv.)
+pivotModelTable <- function(drmodels){
+	compoundNames <- names(drmodels)
+	respVarsNames <- names(drmodels[[1]])
+
+
+	# iterate over all response variables
+	pivotList <- lapply(respVarsNames, FUN = function(resVarName){
+
+		# first gather the list of all affected models
+		resVarPlotData <- lapply(compoundNames, FUN = function(cmpndName){
+			cmpdModels <- drmodels[[cmpndName]]
+			return(cmpdModels[[resVarName]])
+		})
+
+		names(resVarPlotData) <- compoundNames
+
+		return(resVarPlotData)
+	})
+	names(pivotList) <- respVarsNames
+
+	return(pivotList)
+}
+
+# calculate standard error of the mean (SEM)
+sem <- function(x) sqrt(var(x,na.rm=TRUE)/length(na.omit(x)))
+
+
+###############################################################
+###################### Visualization methods #####################
+###############################################################
+
+plotDRModels <- function(drmodels){
+	numResponsVars = length(drmodels)
+	
+	numGridRows = ifelse(numResponsVars > 1, 2, 1)
+	par(mfrow = c(numGridRows, ceiling(numResponsVars/ numGridRows)))
+	
+	lapply(1: numResponsVars, FUN = function(drmodelIndex){
+		responseVarName <- names(drmodels)[drmodelIndex]
+		drmodel = drmodels[[drmodelIndex]]
+		# TODO extract the doseName from the drmodels objects
+		if(!inherits(drmodel, 'drc')){
+			# plot the raw data instead
+			plot(1:10, main= paste('Fitting failed for ', responseVarName))
+					
+		}else{
+			plot(drmodel, type='bars',  col=TRUE, ylab= responseVarName, xlab='Concentration')
+		}
+	})
+} 
+#plotDRModels(drmodels)
+
+
+
+
+visualizeDrModels <- function(plotdata, plotOpts) {
+	numResVars <- length(plotdata)
+	
+	#grid with ratio 3:2 (rows:columns)
+	numGridRows <- ceiling(sqrt(1.5*numResVars))
+	numGridCols <- ceiling(numResVars/numGridRows)
+	
+	doPlotDots <- plotOpts$plotOptions == "Data points"
+	xLab <- plotOpts$plotLabels["xLab"]
+	legendLab <- plotOpts$plotLabels["legendLab"]
+	title <- plotOpts$plotLabels["title"]
+	mName <- plotOpts$mName
+
+	# reshape data
+	rawData <- lapply(names(plotdata), FUN = function(respVar) {
+
+		if(!is.logical(plotdata[[respVar]]$rawdata)) plotdata[[respVar]]$rawdata$respVarName <- respVar
+		if(!is.logical(plotdata[[respVar]]$smoothfitfuns)) {
+			plotdata[[respVar]]$smoothfitfuns$respVarName <- respVar
+			plotdata[[respVar]]$rawdata$plotDots <- doPlotDots
+			} else {
+			plotdata[[respVar]]$rawdata$plotDots <- TRUE
+		}
+		if(!is.logical(plotdata[[respVar]]$errorbardata)) plotdata[[respVar]]$errorbardata$respVarName <- respVar
+
+		plotdata[[respVar]]
+	})
+
+	rVarNames <- names(plotdata)
+	
+	# data frame containing raw data
+	rawDF <- lapply(rawData,"[[", "rawdata")
+	rawDF <- do.call("rbind", rawDF)
+	rawDF$respVarName <- factor(rawDF$respVarName, levels = plotOpts$varOrder)
+
+	# data frame containing fitted data
+	fitDF <- lapply(rawData,"[[", "smoothfitfuns")
+	fitDF <- do.call("rbind", fitDF)
+	if(ncol(fitDF) > 0) fitDF$respVarName <- factor(fitDF$respVarName, levels = plotOpts$varOrder)
+	
+	# data frame containing errorbar data
+	errorDF <- lapply(rawData,"[[", "errorbardata")
+	errorDF <- do.call("rbind", errorDF)
+	if(ncol(errorDF) > 0) errorDF$respVarName <- factor(errorDF$respVarName, levels = plotOpts$varOrder)
+	
+	# new ggplot
+	theme_set(theme_bw(base_size = 13))
+	p <- ggplot(NULL)
+
+	# if errorbars shall be plotted and any errorbardata for plotting exists
+	if( !doPlotDots & ncol(errorDF) > 0) {
+		# plot errorbars
+		p <- p + geom_errorbar(data = errorDF, mapping = aes(x = conc, y = concPredict, ymin = negY, ymax = posY, colour = compound, group = compound), width=0.05) }
+	# plot single data points (if any marked for plotting)
+	if( length(which(rawDF$plotDots == TRUE)) > 0) {
+	p <- p + geom_point(data = subset(rawDF, plotDots == TRUE), mapping = aes(x = conc, y = resVar, colour = compound, group = compound)) }
+
+	# plot dose response curve (if there is any fitting data)
+	if(ncol(fitDF) > 0) {
+	p <- p + geom_line(data = fitDF, mapping = aes(x = conc, y = predictResVar, colour = compound, group = compound)) }
+
+	# set log-scale
+	p <- p + scale_x_log10(breaks = unique(rawDF$conc), labels = formatC(unique(rawDF$conc), digit = 2))
+	# one plot per response variable
+	p <- p + facet_wrap( ~ respVarName, ncol = numGridCols, scales = "free_y")
+	p <- p + ylab("") + ggtitle(paste(title, " (Model: ", mName, ")", sep = ""))
+	if(!is.null(xLab) & !is.na(xLab)) p <- p + xlab(xLab)
+	if(!is.null(legendLab) & !is.na(legendLab)) p <- p + scale_colour_hue(legendLab)
+	print(p) 
+}
+
+
+
+
+#### compile the plot data-structure for a set of componud model for a single response variable
+compilePlotData <- function(cpmdModels, drdata, resVarName, concColIndex, cmpndColIndex, plotOptions){
+
+	#browser()
+  plotSD <- plotOptions == "SD"
+  plotSEM <- plotOptions == "SEM"
+
+	# 1) create the fit data
+
+	# just select the subset which is of interest
+	dataWithFit <- data.frame(conc = drdata[, concColIndex], resVar = drdata[, which(names(drdata) == resVarName)], compound = drdata[, cmpndColIndex])
+
+	# 2) create the smooth model funs
+
+    # sample a set of evaluation points for the predicted function (do it in log-space to get an equal spacing in the visualization later on)
+	# in case the smallest concentration value is zero, set it to 0 + very small value for log10 method. comment: plot looks weird as the zero is very far away from the other data pointd
+    #if(range[1] == 0) range[1] <- 0 + .Machine$double.eps
+
+    if(range(drdata[, concColIndex])[1] == 0) {
+    	drdata <- drdata[drdata[,concColIndex] != 0,]
+    }
+    range <- range(drdata[, concColIndex])
+
+	concRange <-log10(range)
+	predictConcs <- 10^(seq(concRange[1], concRange[2],  (concRange[2] - concRange[1])/100))
+	fitdata <-data.frame(conc = predictConcs)
+
+	#cmpndName  <- names(cpmdModels)[1]
+	smoothFitFunList <- lapply(names(cpmdModels), FUN = function(cmpndName){
+		drmodel <- cpmdModels[[cmpndName]]
+		if(!inherits(drmodel, 'drc')){
+			return(NA)
+		}
+		drPrediction <- data.frame(conc = predictConcs, predictResVar = predict(drmodel, fitdata), compound = cmpndName)
+		return(drPrediction)
+	})
+	smoothFits <- do.call("rbind", smoothFitFunList)
+	smoothFits <- smoothFits[complete.cases(smoothFits),]  # drop rows with na values
+
+	# DEBUG
+	#library(ggplot2)
+	#qplot(smoothFits, aes(x= conc, y= predictResVar, group=compound, color=compound), geom="line")
+	#qplot(conc, predictResVar, data=smoothFits,group=compound, color=compound, geom="line")
+
+	#####
+	# 3) create the error bar data
+	errorBarDataList <- lapply(names(cpmdModels), FUN = function(cmpndName){
+
+		drmodel <- cpmdModels[[cmpndName]]
+		if(!inherits(drmodel, 'drc')){
+			return(NA)
+		}
+
+		drConcs <- unique(drmodel$data$conc)
+
+		errorDF <- data.frame(conc = drConcs)
+		errorDF$concPredict <- predict(drmodel, errorDF)
+
+		ret <- lapply(errorDF$conc, FUN = function(co) {
+			subData <- drmodel$data$meas[which(drmodel$data$conc %in% co)]
+			c(concRespMean = mean(subData), concRespSD = sd(subData), concRespSEM = sem(subData))
+			})
+		ret <- do.call("rbind", ret)
+
+		errorDF <- cbind(errorDF, ret)
+		errorDF$compound <- cmpndName
+		if(plotSD)
+		  errorDF <- transform(errorDF, compound = cmpndName, posY = concPredict + concRespSD, negY = concPredict - concRespSD)
+		else
+		  errorDF <- transform(errorDF, compound = cmpndName, posY = concPredict + concRespSEM, negY = concPredict - concRespSEM)
+
+		errorDF <- errorDF[complete.cases(errorDF),]
+		if(nrow(errorDF) == 0) return(NA)
+
+		return(errorDF)
+	})
+
+	errorBarData <- do.call("rbind", errorBarDataList)
+	errorBarData <- errorBarData[complete.cases(errorBarData),] # drop rows with na values
+
+	plotdata <- list(dataWithFit, smoothFits, errorBarData)
+	names(plotdata) <- c('rawdata', 'smoothfitfuns', 'errorbardata')
+
+	return(plotdata)
+}
+
+
+####
+# reshapes the data to create a list with one sublist for each readout. The sublist will contain 3 datastrucutres (datawithfit, smoothfitfuns, errorbardata). This can than be used to create some nice graphics with ggplot2
+compileDRData4Vis <- function(drmodels, drdata, concIndex, cmpndColIndex, plotOptions){
+	pivModels <- pivotModelTable(drmodels)
+	
+	plotdata <- lapply(names(pivModels), FUN <- function(resVarName){
+		return(compilePlotData(pivModels[[resVarName]], drdata, resVarName, concIndex, cmpndColIndex, plotOptions))
+	})
+	names(plotdata) <- names(pivModels)
+
+	return(plotdata)
+}
+
+
+
+############# playground  ### (make sure to disable before redeloying the node) #####
+
+
+#drmodels <- calcDRModels(drdata, readoutIndices, treatmentIndex, concIndex) 
+#names(drmodels[[1]])
+#names(drmodels)
+
+#
+#library(ggplot2)
+#m <- ggplot(subDRData, aes(y= meas, x=conc, color = treatment)) + geom_point()
+#m + geom_smooth()
+#
+#qplot(meas, data=subDRData, geom='line')
