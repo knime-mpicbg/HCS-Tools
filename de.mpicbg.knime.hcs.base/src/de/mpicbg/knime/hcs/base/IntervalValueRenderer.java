5fc2bf5bc28be5f01428e13a51cb75d6ea934b9c
Antje Janosch <niederle@mpi-cbg.de>
2018-10-23 13:26:56 +0200

    IntervalValueRenderer: cleanup, comments

--- de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/IntervalValueRenderer.java
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/IntervalValueRenderer.java
@@ -9,55 +9,62 @@ import org.knime.core.data.renderer.DataValueRenderer;
 import org.knime.core.data.renderer.DataValueRendererFactory;
 import org.knime.core.data.renderer.DefaultDataValueRenderer;
 
+/**
+ * DataValueRenderer extension for {@link IntervalValue}
+ * IntervalValues will be rendered with human readable format (rounded double values)
+ * 
+ * @author Antje Janosch
+ *
+ */
 @SuppressWarnings("serial")
 public class IntervalValueRenderer extends DefaultDataValueRenderer {
-	
+
 	private NumberFormat m_format = NumberFormat.getNumberInstance(Locale.US);
-	
+
 	public IntervalValueRenderer(String description) {
 		super(description);
-        if (description == null) {
-            throw new IllegalArgumentException("Description must not be null.");
-        }
+		if (description == null) {
+			throw new IllegalArgumentException("Description must not be null.");
+		}
 	}
-	
+
 	/**
     /** Sets the interval values to a human readable format
-     * @param value The value to be rendered.
-     * @see javax.swing.table.DefaultTableCellRenderer#setValue(Object)
-     */
-    @Override
-    protected void setValue(final Object value) {
-    	Object newValue;
-        if (value instanceof IntervalValue) {
-        	
-            IntervalValue cell = (IntervalValue)value;
-            double leftBound = cell.getLeftBound();
-            double rightBound = cell.getRightBound();
-            boolean inclLeft = cell.leftBoundIncluded();
-            boolean inclRight = cell.rightBoundIncluded();
-            
-           String left = m_format != null ? m_format.format(leftBound) : Double.toString(leftBound);
-           String right = m_format != null ? m_format.format(rightBound) : Double.toString(rightBound);
-           
-           String leftIncl = inclLeft ? "[ " : "( ";
-           String rightIncl = inclRight ? " ]" : " )";
-           
-           newValue = (leftIncl + left + " ; " + right + rightIncl);
-           
-        } else {
-            // missing data cells will also end up here
-            newValue = value;
-        }
-        super.setValue(newValue);
-    }
+	 * @param value The value to be rendered.
+	 * @see javax.swing.table.DefaultTableCellRenderer#setValue(Object)
+	 */
+	@Override
+	protected void setValue(final Object value) {
+		Object newValue;
+		if (value instanceof IntervalValue) {
+
+			IntervalValue cell = (IntervalValue)value;
+			double leftBound = cell.getLeftBound();
+			double rightBound = cell.getRightBound();
+			boolean inclLeft = cell.leftBoundIncluded();
+			boolean inclRight = cell.rightBoundIncluded();
+
+			String left = m_format != null ? m_format.format(leftBound) : Double.toString(leftBound);
+			String right = m_format != null ? m_format.format(rightBound) : Double.toString(rightBound);
+
+			String leftIncl = inclLeft ? "[ " : "( ";
+			String rightIncl = inclRight ? " ]" : " )";
+
+			newValue = (leftIncl + left + " ; " + right + rightIncl);
+
+		} else {
+			// missing data cells will also end up here
+			newValue = value;
+		}
+		super.setValue(newValue);
+	}
 
 	/**
 	 * Factory for a {@link IntervalValueRenderer} that shows a human readable format
 	 */
 	public static final class IntervalValueRendererFactory implements DataValueRendererFactory {
 
-		private static final String DESCRIPTION = "my Interval";
+		private static final String DESCRIPTION = "Human Readable";
 
 		@Override
 		public String getDescription() {
