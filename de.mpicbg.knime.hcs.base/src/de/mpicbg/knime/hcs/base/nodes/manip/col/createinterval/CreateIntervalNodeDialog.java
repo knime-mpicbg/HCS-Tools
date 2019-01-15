baf77c5c07038cf27f9d0db2649f33456abf3b49
Antje Janosch <niederle@mpi-cbg.de>
2019-01-15 14:11:04 +0100

    Create Interval: node dialog design (work in progress)

--- de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/manip/col/createinterval/CreateIntervalNodeDialog.java
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/manip/col/createinterval/CreateIntervalNodeDialog.java
@@ -1,11 +1,21 @@
 package de.mpicbg.knime.hcs.base.nodes.manip.col.createinterval;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.Border;
 
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTableSpec;
@@ -34,6 +44,124 @@ public class CreateIntervalNodeDialog extends NodeDialogPane {
 	 */
 	private final JComboBox<String> comp_rightBoundColumn;
 	private DefaultComboBoxModel<String> m_rightBoundColumnModel;
+	
+	private JRadioButton comp_useFixedModes;
+	private JRadioButton comp_useFlexibleModes;
+	
+	
+	/**
+	 * constructor
+	 * inits GUI
+	 */
+	public CreateIntervalNodeDialog() {
+		super();
+		
+		m_settings = new CreateIntervalNodeSettings(CreateIntervalNodeModel.CFG_KEY);
+		
+		// main panel
+		comp_mainPanel = new JPanel(new BorderLayout());
+		
+		// init left bound column combobox
+		m_leftBoundColumnModel = new DefaultComboBoxModel<String>();
+		comp_leftBoundColumn = new JComboBox<String>(m_leftBoundColumnModel);
+		
+		// init left bound column combobox
+		m_rightBoundColumnModel = new DefaultComboBoxModel<String>();
+		comp_rightBoundColumn = new JComboBox<String>(m_rightBoundColumnModel);
+		
+		JPanel northPanel = new JPanel();
+		northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
+		
+		northPanel.add(comp_leftBoundColumn);
+		northPanel.add(comp_rightBoundColumn);
+		
+		JPanel centerPanel = new JPanel(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		comp_useFixedModes = new JRadioButton("set include/exclude flags manually");	
+		centerPanel.add(comp_useFixedModes, c);
+		
+		c.gridx = 1;
+		
+		comp_useFlexibleModes = new JRadioButton("use columns for include/exclude flags");
+		centerPanel.add(comp_useFlexibleModes, c);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(comp_useFlexibleModes);
+		group.add(comp_useFixedModes);
+		group.setSelected(comp_useFixedModes.getModel(), true);
+		
+		JPanel flexibleModesPanel = new JPanel();
+		flexibleModesPanel.setLayout(new BoxLayout(flexibleModesPanel, BoxLayout.Y_AXIS));
+		flexibleModesPanel.setBorder(BorderFactory.createTitledBorder(""));
+		
+		flexibleModesPanel.add(new JLabel("test"));
+		
+		JPanel fixedModesPanel = new JPanel();
+		fixedModesPanel.setLayout(new BoxLayout(fixedModesPanel, BoxLayout.Y_AXIS));
+		fixedModesPanel.setBorder(BorderFactory.createTitledBorder(""));
+		
+		fixedModesPanel.add(new JLabel("test"));
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		
+		centerPanel.add(fixedModesPanel, c);
+		
+		c.gridx = 1;
+		centerPanel.add(flexibleModesPanel, c);
+		
+		comp_mainPanel.add(centerPanel, BorderLayout.CENTER);
+		comp_mainPanel.add(northPanel, BorderLayout.NORTH);
+		
+		
+		comp_useFixedModes.addItemListener(new ItemListener() {	
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.DESELECTED) {
+			        fixedModesPanel.setEnabled(false);
+			        for (Component cp : fixedModesPanel.getComponents() ){
+			        	cp.setEnabled(false);
+			        }
+			    }
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+			        fixedModesPanel.setEnabled(true);
+			        for (Component cp : fixedModesPanel.getComponents() ){
+			        	cp.setEnabled(true);
+			        }
+			    }
+			}
+		});
+		
+		comp_useFlexibleModes.addItemListener(new ItemListener() {	
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.DESELECTED) {
+			        flexibleModesPanel.setEnabled(false);
+			        for (Component cp : flexibleModesPanel.getComponents() ){
+			        	cp.setEnabled(false);
+			        }
+			    }
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+			        flexibleModesPanel.setEnabled(true);
+			        for (Component cp : flexibleModesPanel.getComponents() ){
+			        	cp.setEnabled(true);
+			        }
+			    }
+			}
+		});
+		
+		
+		this.addTab("General Settings", comp_mainPanel);
+	}
 
 	@Override
 	protected void saveSettingsTo(NodeSettingsWO settings) throws InvalidSettingsException {
@@ -77,35 +205,5 @@ public class CreateIntervalNodeDialog extends NodeDialogPane {
 			m_rightBoundColumnModel.setSelectedItem(m_rightBoundColumnModel.getElementAt(0));
 	}
 
-	/**
-	 * constructor
-	 * inits GUI
-	 */
-	public CreateIntervalNodeDialog() {
-		super();
-		
-		m_settings = new CreateIntervalNodeSettings(CreateIntervalNodeModel.CFG_KEY);
-		
-		// main panel
-		comp_mainPanel = new JPanel(new BorderLayout());
-		
-		// init left bound column combobox
-		m_leftBoundColumnModel = new DefaultComboBoxModel<String>();
-		comp_leftBoundColumn = new JComboBox<String>(m_leftBoundColumnModel);
-		
-		// init left bound column combobox
-		m_rightBoundColumnModel = new DefaultComboBoxModel<String>();
-		comp_rightBoundColumn = new JComboBox<String>(m_rightBoundColumnModel);
-		
-		JPanel northPanel = new JPanel();
-		northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
-		
-		northPanel.add(comp_leftBoundColumn);
-		northPanel.add(comp_rightBoundColumn);
-		
-		comp_mainPanel.add(northPanel, BorderLayout.NORTH);
-		//comp_mainPanel.add(comp_rightBoundColumn, BorderLayout.NORTH);
-		
-		this.addTab("General Settings", comp_mainPanel);
-	}
+
 }
