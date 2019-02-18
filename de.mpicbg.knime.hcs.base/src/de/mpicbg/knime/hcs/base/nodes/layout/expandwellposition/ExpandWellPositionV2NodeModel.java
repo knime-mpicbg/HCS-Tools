c9105ff13025e39fc28cc44dcfbbd285e5d34570
Antje Janosch <niederle@mpi-cbg.de>
2019-02-18 16:09:18 +0100

    Expand Well Position: configure method, work in progress

--- de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/layout/expandwellposition/ExpandWellPositionV2NodeModel.java
+++ de.mpicbg.knime.hcs.base/src/de/mpicbg/knime/hcs/base/nodes/layout/expandwellposition/ExpandWellPositionV2NodeModel.java
@@ -1,6 +1,11 @@
 package de.mpicbg.knime.hcs.base.nodes.layout.expandwellposition;
 
+import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.DoubleValue;
+import org.knime.core.data.StringValue;
+import org.knime.core.data.container.ColumnRearranger;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
@@ -41,8 +46,47 @@ public class ExpandWellPositionV2NodeModel extends AbstractNodeModel {
 
 	@Override
 	protected DataTableSpec[] configure(DataTableSpec[] inSpecs) throws InvalidSettingsException {
-		// TODO Auto-generated method stub
-		return super.configure(inSpecs);
+		
+		DataTableSpec inSpec = inSpecs[0];
+		String selectedColumn = ((SettingsModelString) this.getModelSetting(CFG_WELL_COLUMN)).getStringValue();
+		boolean deleteSource = ((SettingsModelBoolean) this.getModelSetting(CFG_DELETE_SOURCE)).getBooleanValue();
+		boolean convertRowValues = ((SettingsModelBoolean) this.getModelSetting(CFG_CONVERT_ROWVALS)).getBooleanValue();
+		
+		// autoguess cplumn if not yet available
+		if(selectedColumn == null) {
+			for(DataColumnSpec cSpec : inSpec) {
+				if(cSpec.getType().isCompatible(StringValue.class)) {
+					selectedColumn = cSpec.getName();
+					setWarningMessage("Autoguessing column with well strings: \"" + cSpec.getName() + "\"");
+					this.updateModelSetting(CFG_WELL_COLUMN, new SettingsModelString(CFG_WELL_COLUMN, selectedColumn));
+				}
+			}
+		}
+		
+		// check for availability
+		if(!inSpec.containsName(selectedColumn))
+			throw new InvalidSettingsException("Input table does not contain the expected columns: " + selectedColumn);
+		
+		// Saving index of column and row into variable idCol and idRow
+		int colIdx = inSpec.findColumnIndex(selectedColumn);
+		
+		// check for correct data type
+		DataType t = inSpec.getColumnSpec(colIdx).getType();
+		if(!(t.isCompatible(StringValue.class) || t.isCompatible(DoubleValue.class)))
+			throw new InvalidSettingsException("Column \"" + selectedColumn + "\" must not be of type \"" + t.getName() +"\".");
+	
+		
+		ColumnRearranger rearranged_table = createColumnRearranger(inSpec, colIdx, deleteSource, convertRowValues);
+		DataTableSpec outSpec = rearranged_table.createSpec();
+
+		return new DataTableSpec[]{outSpec};	
+	}
+
+	private ColumnRearranger createColumnRearranger(DataTableSpec inSpec, int colIdx, boolean deleteSource,
+			boolean convertRowValues) {
+		ColumnRearranger rearranger = new ColumnRearranger(inSpec);
+		
+		return rearranger;
 	}
 
 	@Override
