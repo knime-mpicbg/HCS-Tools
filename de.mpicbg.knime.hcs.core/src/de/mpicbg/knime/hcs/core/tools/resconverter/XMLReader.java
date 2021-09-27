fbfa10cf5e8fbec9692678f908fb6f2059fd08a1
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2021-09-27 11:09:35 +0200

    Refine imports in XMLreader

--- de.mpicbg.knime.hcs.core/src/de/mpicbg/knime/hcs/core/tools/resconverter/XMLReader.java
+++ de.mpicbg.knime.hcs.core/src/de/mpicbg/knime/hcs/core/tools/resconverter/XMLReader.java
@@ -1,14 +1,19 @@
 package de.mpicbg.knime.hcs.core.tools.resconverter;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /* Test-Application */
 /* --------------------------------------------------------------------------- */
